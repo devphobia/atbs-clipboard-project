f05abe7b49863975fe3bb7f5db488867fb66cdf3
Phobet <unigabrielcb@protonmail.com>
2021-12-17 15:51:13 -0300

    Added two new optional parameters for business messages and related tasks.

--- mclip.py
+++ mclip.py
@@ -3,20 +3,47 @@
 import sys, time, pyperclip
 
 ANSWERS = {
-    "together" : "https://youtu.be/yPYZpwSpKmA"
+    "gaming" : "Yep. It's gaming time.",
+    "sheets" : "Here are the sheets you wanted."
     }
 
 def invalid_usage():
-    print("Usage: python mclip.py [keyphrase] - copy phrase text")
+    print("Usage: python mclip.py [keyphrase] [client_name (optional)] [intro (optional)] - copy phrase text")
     time.sleep(1)
     sys.exit()
+    
 
-if len(sys.argv) != 2:
-    print("Invalid number of arguments!")
+def add_work_intro(text, client_name):
+    intro = f"Good evening, {client_name}!\n"
+    outro = f"\nMany thanks,\ndevphobia"
+
+    new_text = intro + text + outro
+
+    return new_text
+
+
+while True:
+    if len(sys.argv) < 4:
+        sys.argv.append(None)
+    else:
+        break
+
+if len(sys.argv) == 4:
+    keyphrase, client_name, intro = sys.argv[1::]
+else:
     invalid_usage()
-elif sys.argv[1] not in ANSWERS:
-    print("Invalid keyphrase!")
 
-keyphrase = sys.argv[1]     #first command line = keyphrase
-pyperclip.copy(ANSWERS[keyphrase])
-print(f"{keyphrase} answer copied to clipboard!")
+if keyphrase == None:
+    invalid_usage()
+elif keyphrase not in ANSWERS:
+    print("Keyphrase not found!")
+else:
+    answer = ANSWERS[keyphrase]
+
+if intro != None:
+    answer = add_work_intro(answer, client_name)
+
+pyperclip.copy(answer)
+
+print(f"{keyphrase} answer copied to the clipboard!")
+
